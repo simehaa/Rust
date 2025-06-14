18b114f6a003693d5bc61931400c7ec1ed7c3bc1
Simen Håpnes <simen.haapnes@gmail.com>
2025-06-14 21:23:41 +0200

    Chpt. 18: blog post handler using object-oriented state pattern

---
+++ blog_post_state_pattern/src/main.rs
@@ -0,0 +1,147 @@
+fn main() {
+    let mut post = Post::new();
+    assert_eq!(post.content(), "");
+
+    let txt = "Hello, World!";
+    post.add_text(txt);
+    assert_eq!(post.content(), ""); // draft state: content not available
+    
+    post.request_review();
+    assert_eq!(post.content(), ""); // pending review state: content not available
+    
+    post.approve();
+    assert_eq!(post.content(), ""); // pending second review state: content not available
+    
+    post.approve();
+    println!("Post approved and published:\n{}", post.content());
+    assert_eq!(post.content(), txt);
+
+    post.add_text("\nHello, Mars!");
+    assert_eq!(post.content(), txt); // content not modifiable unless the state is draft
+}
+
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
+    }
+
+    pub fn add_text(&mut self, text: &str) {
+        if self.state.as_ref().unwrap().can_add_text() {
+            self.content.push_str(text);
+        }
+    }
+
+    pub fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(self)
+    }
+
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
+
+    pub fn reject(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.reject())
+        }
+    }
+}
+
+trait State {
+    fn can_add_text(&self) -> bool {
+        false // default false, for most states, except Draft which will overwrite
+    }
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn reject(self: Box<Self>) -> Box<dyn State>;
+
+    fn content<'a>(&self, _post: &'a Post) -> &'a str {
+        ""
+    }
+}
+
+struct Draft {}
+
+impl State for Draft {
+    fn can_add_text(&self) -> bool {
+        true
+    }
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+
+struct PendingReview {}
+
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview2 {})
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Draft {})
+    }
+}
+
+struct PendingReview2 {}
+
+impl State for PendingReview2 {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Draft {})
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
+    }
+}
